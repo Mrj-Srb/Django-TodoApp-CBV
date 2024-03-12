9d70a5cd14fb1545871e11995d946e33752957cb
Meraj Sarab <merajpython@gmail.com>
2024-03-12 14:59:05 +0330

    Create and Include accounts urls.py
    add login, logout, register urls in accounts/urls.py

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from accounts import views
+
+from django.contrib.auth.views import LogoutView
+
+
+urlpatterns = [
+    path('login/',views.CustomLoginView.as_view(),name='login'),
+    path('logout/',LogoutView.as_view(next_page="/"),name='logout'),
+    path('register/',views.RegisterView.as_view(),name='register')
+
+]
